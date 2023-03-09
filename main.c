7dd25da7fcebfdb0c19d817a927a1dc1c303336b
Kivler <113400157+Kivler@users.noreply.github.com>
2023-03-09 14:00:30 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+    FILE* fp;
+    int a[] = {1, 2, 3};
+    char b[] = "ABC";
+    float c [] = {1.1,1.2,1.3};
+    if ((fp = fopen("cj.txt","wb+")) == NULL){
+        printf("Cannot open the file ...");
+        exit(1);
+    }
+    if(fwrite(a),sizeof(a),1,fp)!=1){
+        printf("File write error...\n");
+    }
+    fseek(fb, 0 , SEEK_SET);
+    if(fwrite(c),sizeof(c), 1, fp)!=1){
+        printf("File write error...\n");
+    }
+    
+    return 0;
+}
