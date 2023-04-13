5508aeba2e4083f89eea51eceb913ea18fe7f1f8
Kivler <113400157+Kivler@users.noreply.github.com>
2023-04-13 14:26:32 +0800

    Update main.c

--- lab6/main.c
+++ lab6/main.c
@@ -1,16 +1,33 @@
-
 #include <stdio.h>
-int multiplication(int i,int j){
 
-    
-    
-    
+int multi (int i, int j)
+{
+    if (i==1)
+    {
+        if (j==1)
+        {
+            printf("%d*%d=%-2d\n", i, j, i*j);
+        }
+        else
+        {
+            printf("%d*%d=%-2d\n", i, j-1, i*(j-1));
+        }
+    }
+    else
+    {
+        if (j==1)
+        {
+            printf("%d*%d=%-2d\n", i, j, i*j);
+        }
+        else
+        {
+            printf("%d*%d=%-2d\n", i, j-1, i*(j-1));
+        }
+    }
 }
-
-
 
 int main()
 {
-    multiplication(1,1);
+    multi(9, 9);
     return 0;
 }
